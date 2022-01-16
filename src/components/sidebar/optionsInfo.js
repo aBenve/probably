9d125538b7ecc849a8dc9178204aa3ac9996f904
aBenve <abenvenuto@itba.edu.ar>
2022-01-16 18:53:43 -0300

    All discrete options working (hypergeometric triple slider is not done yet) Also fix max values for discrete distros.

--- src/components/sidebar/optionsInfo.js
+++ src/components/sidebar/optionsInfo.js
@@ -50,13 +50,38 @@ let HypergeometricMean = (inputs) => {
 let HypergeometricVariance = (inputs) => {
     return inputs[0] * inputs[1] / inputs[2] * (1- inputs[1]/inputs[2])*(inputs[2]-inputs[0])/(inputs[2]-1)
 }
-let PoissonMean = (inputs) => {
-    return inputs[0]
-}
-let PoissonVariance = (inputs) => {
-    return inputs[0]
+
+let HypereometricLowerValue = (inputs) => {
+    return Math.max(1, GeometricMean(inputs) - 5 * generalSD(GeometricVariance(inputs)))
 }
 
+let HypereometricUpperValue = (inputs) => {
+    let lower = GeometricLowerValue(inputs)
+    let toRet = Math.ceil(GeometricMean(inputs) + 5 * generalSD(GeometricVariance(inputs)))
+    if(toRet - lower < 4) return lower + 4
+    return toRet
+}
+
+let PoissonMean = (inputs) => {
+    return Math.floor(inputs[0])
+}
+let PoissonVariance = (inputs) => {
+    return Math.floor(inputs[0])
+}
+
+let PoissonLowerValue = (inputs) => {
+    return Math.floor(Math.max(1, PoissonMean(inputs) - 4 * generalSD(PoissonVariance(inputs))))
+}
+
+let PoissonUpperValue = (inputs) => {
+    if(inputs[0] < 0.5)
+        return 4
+    if(inputs[0] >= 0.5 && inputs[0] < 1)
+        return 6
+    if(inputs[0] >= 1 && inputs[0] < 20)
+        return Math.floor(PoissonMean(inputs) + 5 * generalSD(PoissonVariance(inputs)))
+    return Math.floor(PoissonMean(inputs) + 4 * generalSD(PoissonVariance(inputs)))
+}
 
 
 /*
@@ -84,7 +109,7 @@ const userOptions = [
 
         type: "Discrete",
         related: false,
-        inputs: [{ label:"Trials", step:1, value:0}, {label:"Probability of success", step:0.001, value:0}],
+        inputs: [{ label:"Trials", step:1, value:0, max:500}, {label:"Probability of success", step:0.001, value:0, max:1}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -101,11 +126,12 @@ const userOptions = [
             return  inputs[0] * Math.pow(1 - inputs[0], x - 1)
         },
 
+
         color:"#F694FE",
         accentColor:"#F267FE",
         type: "Discrete",
         related: false,
-        inputs: [{label:"Probability of success", step:0.001, value:0}],
+        inputs: [{label:"Probability of success", step:0.001, value:0, max:1}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -115,6 +141,9 @@ const userOptions = [
         variance:(inputs) => {HypergeometricVariance(inputs)},
         sd:(inputs) => {generalSD(HypergeometricVariance(inputs))},
 
+        lowerValue:(inputs) => { return HypereometricLowerValue(inputs)},
+        upperValue:(inputs) => { return HypereometricUpperValue(inputs)},
+
         P:(inputs, x) => {
             return (combinations(inputs[1], x) * combinations(inputs[2] - inputs[1], inputs[2]-x)/combinations(inputs[2],inputs[0]))
         },
@@ -123,7 +152,7 @@ const userOptions = [
         accentColor:"#FE6767",
         type: "Discrete",
         related: false,
-        inputs: [{label:"Sample size", step:1, value:0}, {label:"Total number of objects", step:1, value: 0}, {label:"Amount of successes", step:1, value: 0}],
+        inputs: [{label:"Sample size", step:1, value:0, max:500}, {label:"Total number of objects", step:1, value: 0, max:500}, {label:"Amount of successes", step:1, value: 0, max:500}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -133,6 +162,9 @@ const userOptions = [
         variance:(inputs) => {PoissonVariance(inputs)},
         sd:(inputs) => {generalSD(PoissonVariance(inputs))},
 
+        lowerValue:(inputs) => { return PoissonLowerValue(inputs)},
+        upperValue:(inputs) => { return PoissonUpperValue(inputs)},
+
         P:(inputs, x) => {
             return  Math.exp(-inputs[0]) * Math.pow(inputs[0], x) / factorial(x)
         },
@@ -141,7 +173,7 @@ const userOptions = [
         accentColor:"#FECB67",
         type: "Discrete",
         related: false,
-        inputs: [{label:"λ ratio", step: 1, value: 0}],
+        inputs: [{label:"λ ratio", step: 1, value: 0, max:150}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -159,7 +191,7 @@ const userOptions = [
         accentColor:"#85E97C",
         type: "Continuous",
         related: true,
-        inputs: [{label:"Population", step:1, value: 0}, {label:"Positive cases", step: 1, value: 0}],
+        inputs: [{label:"Population", step:1, value: 0, max:500}, {label:"Positive cases", step: 1, value: 0, max:500}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -177,7 +209,7 @@ const userOptions = [
         accentColor:"#67B6FE",
         type: "Continuous",
         related: false,
-        inputs: [{label:"λ ratio", step:1, value: 0}],
+        inputs: [{label:"λ ratio", step:1, value: 0, max:500}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -195,7 +227,7 @@ const userOptions = [
         accentColor:"#67FEE3",
         type: "Continuous",
         related: false,
-        inputs: [{label:"Mean μ", step:1, value: 0}, {label:"Standard deviation σ", step: 1, value: 0}],
+        inputs: [{label:"Mean μ", step:1, value: 0, max:500}, {label:"Standard deviation σ", step: 1, value: 0, max:500}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -213,7 +245,7 @@ const userOptions = [
         accentColor:"#FBFE67",
         type: "Continuous",
         related: false,
-        inputs: [{label:"Mean μ", step: 1, value: 0}, {label:"Standard deviation σ", step: 1, value: 0}],
+        inputs: [{label:"Mean μ", step: 1, value: 0, max:500}, {label:"Standard deviation σ", step: 1, value: 0, max:500}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
     {
@@ -231,7 +263,7 @@ const userOptions = [
         accentColor:"#FE67C2",
         type: "Continuous",
         related: false,
-        inputs: [{label:"Degrees of freedom", step: 1, value: 0}],
+        inputs: [{label:"Degrees of freedom", step: 1, value: 0, max:500}],
         description: "Lorem ipsum dolor sit amet. Et quod dolor ut eveniet autem sit quos quidem 33 illum magnam. Id galisum vero quo quidem galisum At nesciunt sapiente non blanditiis corrupti qui eligendi nemo. Eum nostrum repudiandae sed facere laudantium sit facilis asperiores?\n"
     },
 ]
