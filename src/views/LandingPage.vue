30a575f774b080b2f8dc1d3d5a84bfca68cbbe63
aBenve <abenvenuto@itba.edu.ar>
2022-03-01 19:28:23 -0300

    Landing button color changed.

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -30,17 +30,6 @@ export default {
 
 }
 
-@keyframes gradient {
-  0% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-}
 .button:hover{
   opacity: 1;
 }
